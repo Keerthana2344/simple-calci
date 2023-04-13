641b541f0b9230be2ffc223056aabaaa81da06ae
Keerthana2344 <129586268+Keerthana2344@users.noreply.github.com>
2023-04-13 10:24:36 +0000

    Done

--- main.py
+++ main.py
@@ -28,9 +28,51 @@ def add():
   num1=int(t1.get())
   num2=int(t2.get())
   sum=num1 + num2
-  t3.insert(END,str(sum))
+  t1.insert(END,str(sum))
 
 b1=Button(window, text="Add",command=add)
 b1.place(x=50,y=200)
+
+def sub():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  sub=num1 - num2
+  t2.insert(END,str(sub))
+
+b2=Button(window, text="Subract",command=sub)
+b2.place(x=100,y=200)
+
+def mul():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  pro=num1 * num2
+  t3.insert(END,str(pro))
+
+b3=Button(window, text="Multi",command=mul)
+b3.place(x=150,y=200)
+
+def div():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  quo=num1 / num2
+  t3.insert(END,str(quo))
+
+b4=Button(window, text="Divide",command=div)
+b4.place(x=200,y=200)
+
+def clear():
+  t1.delete(0,END)
+  t2.delete(0,END)
+  t3.delete(0,END)
+b5=Button(window, text="Clear",command=clear)
+b5.place(x=130,y=250)
+
+
+
+
+
+
+
+
 
 
